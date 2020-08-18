82f998ee63bc9aedec9a696042210f841c9768de
Lauren Emick <laurenemick6@gmail.com>
2020-08-18 13:04:22 -0700

    problem importing EnableWebSecurity annotation

---
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.shoppingcart.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig
+{
+}
