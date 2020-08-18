a290b14f24d1009f2a9394ad6ef5dd00a2642281
Lauren Emick <laurenemick6@gmail.com>
2020-08-18 15:03:29 -0700

    imports just started working

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/config/SecurityConfig.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.lambdaschool.shoppingcart.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig
 {
 }
