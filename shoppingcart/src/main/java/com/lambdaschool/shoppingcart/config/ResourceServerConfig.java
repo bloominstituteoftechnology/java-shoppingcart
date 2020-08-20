b2b474dd13c0ae469c51b38c43714fd211ac0719
martinezmarina <martinezmarina980@gmail.com>
2020-08-20 19:31:11 -0400

    resolved

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/config/ResourceServerConfig.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/config/ResourceServerConfig.java
@@ -1,28 +1,42 @@
 package com.lambdaschool.shoppingcart.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 
+/**
+ * Once the client has gained authorization, users need to gain authentication. This class is response for handling that.
+ * It also configures which roles have access to which endpoints. So controls the users' access!
+ */
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+public class ResourceServerConfig
+        extends ResourceServerConfigurerAdapter
+{
     private static final String RESOURCE_ID = "resource_id";
 
     @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId(RESOURCE_ID).stateless(false);
+    public void configure(ResourceServerSecurityConfigurer resources)
+    {
+        resources.resourceId(RESOURCE_ID)
+                .stateless(false);
     }
 
+    /**
+     * This method configures which roles can access which endpoints
+     *
+     * @param http Our HttpSecurity object that is maintains by Spring
+     * @throws Exception in case the configurations fails
+     */
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http.logout().disable();
-
+    public void configure(HttpSecurity http)
+            throws
+            Exception
+    {
         http.authorizeRequests()
                 .antMatchers("/",
                         "/h2-console/**",
@@ -33,24 +47,36 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                         "/webjars/**",
                         "/createnewuser")
                 .permitAll()
-                .antMatchers("/users/users",
-                        "/users/user",
-                        "/users/user/**",
-                        "/carts/cart/**",
-                        "/products/products",
-                        "/products/product",
-                        "/products/product/**"
-                )
-                .hasRole("ADMIN")
-                .antMatchers("/logout",
-                        "/carts/user",
-                        "/carts/create/product",
-                        "/carts/update/cart/**",
-                        "/carts/delete/**",
-                        "/users/myinfo")
-                .authenticated()
+                .antMatchers("/users/users", "/users/user/**", "/carts/cart/**").hasAnyRole("ADMIN")
+                .antMatchers("/carts/user/**", "/carts/create/**", "/users/myinfo").authenticated()
+                .antMatchers("/carts/update/**", "/carts/delete/**", "/products/**").hasAnyRole("ADMIN")
+//                .antMatchers(HttpMethod.POST,
+//                        "/users/**")
+//                .hasAnyRole("ADMIN")
+//                .antMatchers(HttpMethod.DELETE,
+//                        "/users/**")
+//                .hasAnyRole("ADMIN")
+//                .antMatchers(HttpMethod.PUT,
+//                        "/users/**")
+//                .hasAnyRole("ADMIN")
+//                .antMatchers("/useremails/**",
+//                        "/oauth/revoke-token",
+//                        "/logout")
+//                .authenticated()
+//                .antMatchers("/roles/**")
+//                .hasAnyRole("ADMIN")
                 .and()
                 .exceptionHandling()
                 .accessDeniedHandler(new OAuth2AccessDeniedHandler());
+
+        http.csrf()
+                .disable();
+
+        http.headers()
+                .frameOptions()
+                .disable();
+
+        http.logout()
+                .disable();
     }
 }
