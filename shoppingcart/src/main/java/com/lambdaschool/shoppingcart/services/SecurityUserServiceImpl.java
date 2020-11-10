d3346d221d48f0d3cf4d113d46a143ddd2190ae4
Simone Ballard <simone.a.ballard@gmail.com>
2020-11-10 16:47:05 -0700

    security dependencies added

---
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/services/SecurityUserServiceImpl.java
@@ -0,0 +1,24 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Transactional
+@Service(value = "securityUserService")
+public class SecurityUserServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username.toLowerCase());
+        if (user == null) {
+            throw new ResourceNotFoundException("Invalid username or password");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getAuthority());
+    }
+}
