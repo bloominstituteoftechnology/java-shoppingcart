9fc44b0af26c7455d3278db894faabc042fe9f91
Simone Ballard <simone.a.ballard@gmail.com>
2020-11-12 19:13:47 -0700

    final tests and deployed

--- shoppingcart/src/test/java/com/lambdaschool/shoppingcart/controllers/CartControllerTest.java
+++ shoppingcart/src/test/java/com/lambdaschool/shoppingcart/controllers/CartControllerTest.java
@@ -1,17 +1,31 @@
 package com.lambdaschool.shoppingcart.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lambdaschool.shoppingcart.ShoppingCartApplication;
+import com.lambdaschool.shoppingcart.models.CartItem;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShoppingCartApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CartControllerTest
 {
+    @Autowired
+    CartItemService cartItemService;
 
     @Before
     public void setUp() throws Exception
     {
+        MockitoAnnotations.initMocks(this);
     }
 
     @After
@@ -22,15 +36,39 @@ public class CartControllerTest
     @Test
     public void listCartItemsByUserId()
     {
+        String apiUrl = "/users/user/{userid}";
+        Mockito.when(cartItemService.findAll()).thenReturn(cartItemService);
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb).andReturn();
+        String testResult = r.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String expectedResult = mapper.writeValueAsString(cartItemList);
+
+        assertEquals(expectedResult, testResult);
+
+        //not sure this probably has extra code since we aren't using mock data like above^^
     }
 
     @Test
     public void addToCart()
     {
+        for (int i = 0; i < 17; i++)
+        {
+            cartItemService.addToCart(u3.getUserid(),
+                p3.getProductid(), "added via seed data");
+        }
+
+        CartItem addCartItem = cartItemService.save(p3);
+        assertNotNull(addCartItem);
+        assertEquals(p3, addCartItem.getProduct());
     }
 
     @Test
     public void removeFromCart()
     {
+        cartItemService.delete(4);
+        assertEquals(2, cartItemService.findAll().size());
     }
 }
--- shoppingcart/src/test/java/com/lambdaschool/shoppingcart/services/CartItemServiceImplTest.java
+++ shoppingcart/src/test/java/com/lambdaschool/shoppingcart/services/CartItemServiceImplTest.java
@@ -13,46 +13,15 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ShoppingCartApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CartItemServiceImplTest
 {
     @Autowired
-    private CartItemService cartItemService;
-
-    @MockBean
-    private CartItemRepository cartItemrepos;
-
-    @MockBean
-    HelperFunctions helperFunctions;
-
-    private List<CartItem> cartItemList;
-
+    CartItemService cartItemService;
 
     @Before
     public void setUp() throws Exception
     {
-        cartItemList = new ArrayList<>();
-
-        Item i1 = new Item("book1");
-        i1.setItemid(1);
-        Item i2 = new Item("book2");
-        i2.setItemid(2);
-        Item i3 = new Item("book3");
-        i3.setItemid(3);
-
-        User u1 = new User("testuser");
-        u1.getItems()
-            .add(new UserItems(u1,
-                i1));
-        u1.getItems()
-            .add(new UserItems(u1,
-                i2));
-        u1.getItems()
-            .add(new UserItems(u1,
-                i3));
-
-        u1.setUserid(101);
-        cartItemList.add(u1);
-
         MockitoAnnotations.initMocks(this);
     }
 
@@ -64,33 +33,20 @@ public class CartItemServiceImplTest
     @Test
     public void addToCart() throws Exception
     {
-        String apiUrl = "/users/user/{itemid}";
+        for (int i = 0; i < 17; i++)
+        {
+            cartItemService.addToCart(u3.getUserid(),
+                p3.getProductid(), "added via seed data");
+        }
 
-        Mockito.when(cartItemService.update(any(CartItem.class),
-            any(Long.class)))
-            .thenReturn(cartItemList.get(0));
-
-        RequestBuilder rb = MockMvcRequestBuilders.put(apiUrl,
-            100L)
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON)
-            .content("{\"cartitem\": \"book1\"}");
-
-        mockMvc.perform(rb)
-            .andExpect(status().is2xxSuccessful())
-            .andDo(MockMvcResultHandlers.print());
-    }
+        CartItem addCartItem = cartItemService.save(p3);
+        assertNotNull(addCartItem);
+        assertEquals(p3, addCartItem.getProduct());
 
     @Test
     public void removeFromCart() throws Exception
     {
-        String apiUrl = "/users/user/{itemid}";
-
-        RequestBuilder rb = MockMvcRequestBuilders.delete(apiUrl,
-            "3")
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON);
-        mockMvc.perform(rb)
-            .andExpect
+        cartItemService.delete(4);
+        assertEquals(2, cartItemService.findAll().size());
     }
 }
