178ee8959fb26e0263a773c87f416d581edaa1f9
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-04-20 18:22:13 -0400

    commit

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/User.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/User.java
@@ -1,12 +1,14 @@
 package com.lambdaschool.shoppingcart.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * The entity allowing interaction with the users table
@@ -122,12 +124,17 @@ public class User
     {
         return username;
     }
+    public void setPassword(String password)
+    {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
 
-    /**
-     * setter for username
-     *
-     * @param username the new username (String) converted to lowercase
-     */
+     public void setPasswordNoEncrypt(String password)
+     {
+     this.password = password;
+     }
+
     public void setUsername(String username)
     {
         this.username = username.toLowerCase();
@@ -168,10 +175,6 @@ public class User
      *
      * @param password the new password (String) for the user
      */
-    public void setPassword(String password)
-    {
-        this.password = password;
-    }
 
     /**
      * Getter for user role combinations
@@ -211,5 +214,15 @@ public class User
     public void setCarts(Set<CartItem> carts)
     {
         this.carts = carts;
+    }
+
+    @JsonIgnore
+    public List<SimpleGrantedAuthority> getAuthority(){
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+        for (UserRoles ur: this.roles){
+           String myRole = "ROLE" + ur.getRole().getName().toUpperCase();
+           rtnList.add(new SimpleGrantedAuthority(myRole));
+        }
+        return rtnList;
     }
 }
